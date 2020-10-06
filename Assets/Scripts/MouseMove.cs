029376297c4034f48619ab8eaffd492dca806652
settawut-niy <settawut.niy@gmail.com>
2020-10-06 16:21:21 +0700

    Deleted unused assets, created sample water, circle shader and created a script to control circle shader
    - MouseMove Class
          - Use to control white circle shader by mouse position
    
    - Other
          - Created 2 type of shader (Water, Circle) to find the solution
          - Deleted unused assets from initial imported project

---
+++ Assets/Scripts/MouseMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMove : MonoBehaviour
+{
+    Renderer myRenderer;
+    RaycastHit hit;
+
+    bool isShowCircle = false;
+
+
+    void Awake()
+    {
+        myRenderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+            myRenderer.material.SetVector("_CirclePosition", new Vector4(hit.point.x, 0, hit.point.z, 0));
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isShowCircle)
+            {
+                isShowCircle = true;
+                myRenderer.material.SetFloat("_IsShowCircle", isShowCircle ? 1f : 0f);
+            }
+            else
+            {
+                isShowCircle = false;
+                myRenderer.material.SetFloat("_IsShowCircle", isShowCircle ? 1f : 0f);
+            }
+
+        }
+    }
+}
